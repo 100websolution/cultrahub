adf06e83ba0ba56e80c3893c12a38ea664235786
Mitkar Ghosh <100websolution@gmail.com>
2018-04-11 02:14:38 +0530

    hi

--- wp-content/themes/cultrahub/functions.php
+++ wp-content/themes/cultrahub/functions.php
@@ -996,7 +996,7 @@ function cultrahub_sharethoughts(){
 			$message1 .= 'Mood: '.$mood_sharethought.'<br />';
 			
 			$headers1 = 'Content-Type: text/html; charset=UTF-8';
-			$headers1 .= 'From: Cultrahub < '. $_POST['email_sharethought'] .' >';
+			$headers1 .= 'From: Cultrahub < info@techtimes-in.com >';
 			wp_mail( '100websolution@gmail.com', 'Share Your Thoughts', $message1, $headers1 );
 			
 			echo 'success';
