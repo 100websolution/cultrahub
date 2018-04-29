a374f5236a0e64f2508eb41e40454a7d5c2a1076
eshasaha90 <esha.info90@users.noreply.github.com>
2018-04-29 22:27:58 +0530

    about

--- wp-content/themes/cultrahub/template-about.php
+++ wp-content/themes/cultrahub/template-about.php
@@ -5,7 +5,7 @@ Template Name: About
 get_header('culture');
 ?>
 <!--MAIN CONTAINER START-->
-<div class="mainContainer" id="mainContainer">	
+<div class="mainContainer" id="mainContainer">
 	<div class="section">
 		<div class="container">
 			<hr class="mb35 mt0">
@@ -26,7 +26,56 @@ get_header('culture');
                 <div class="heading center">Connecting Millions Of Buyers And Sellers From <br>Around The World.</div>
                 <div class="connectingBlock">
                     <div class="connectingRight">
-                        
+                        <ul class="ul row">
+                            <li class="col50">
+                                <div class="aboutIconBox">
+                                    <div class="aiIcon">
+                                        <img src="<?php echo get_template_directory_uri();?>/images/icon_adv_technology.png" alt="">
+                                    </div>
+                                    <div class="subheading2">The Most Advanced Technology</div>
+                                    <div class="aiText">
+                                        <p>Using the very latest technology we keep our site at the cutting edge of innovation, insuring speed, ease of use and uptime</p>
+                                    </div>
+                                    <div class="clear"></div>
+                                </div>
+                            </li>
+                            <li class="col50">
+                                <div class="aboutIconBox">
+                                    <div class="aiIcon">
+                                        <img src="<?php echo get_template_directory_uri();?>/images/icon_24_7_security.png" alt="">
+                                    </div>
+                                    <div class="subheading2">24/7 <br>Secure Platform</div>
+                                    <div class="aiText">
+                                        <p>Our highly specialist engineer teams work around the clock to protect your shop and data from cyber threats and criminals.</p>
+                                    </div>
+                                    <div class="clear"></div>
+                                </div>
+                            </li>
+                            <li class="col50">
+                                <div class="aboutIconBox">
+                                    <div class="aiIcon">
+                                        <img src="<?php echo get_template_directory_uri();?>/images/icon_corporate.png" alt="">
+                                    </div>
+                                    <div class="subheading2">Corporate Responsibilities</div>
+                                    <div class="aiText">
+                                        <p>Our code of conduct and ethical stand ensures we leave a positive impact on society, environment and the world at large.</p>
+                                    </div>
+                                    <div class="clear"></div>
+                                </div>
+                            </li>
+                            <li class="col50">
+                                <div class="aboutIconBox">
+                                    <div class="aiIcon">
+                                        <img src="<?php echo get_template_directory_uri();?>/images/icon_devices.png" alt="">
+                                    </div>
+                                    <div class="subheading2">Available In Any Device</div>
+                                    <div class="aiText">
+                                        <p>Our site seamlessly adapts to any device, giving you the best possible experience regardless of the technology you are using</p>
+                                    </div>
+                                    <div class="clear"></div>
+                                </div>
+                            </li>
+                        </ul>
                     </div>
                     <div class="connectingLeft">
                         <p>Cultrahub was founded in 2015 and is the world’s first social ecommerce marketplace designed to facilitate in-culture and cross-cultural interactions through the exchange of products, services, and information.</p>
@@ -36,7 +85,204 @@ get_header('culture');
                 </div>
             </div>
             
-			<div class="innerContainer mt70">
+            <div class="innerContainer mt80">
+                <div class="heading center">The Cultrahub Team</div>
+                <div class="heading_tag">Cultrahub values all cultures and the range of diversity everyone brings to the table. For us, that starts with our staff. Get to know who is part of our team and connect with them on a personal level.</div>
+                <div class="teamList">
+                    <ul class="ul row">
+                        <li class="col33">
+                            <div class="teamBox">
+                                <div class="teamImg">
+                                    <img src="<?php echo get_template_directory_uri();?>/images/team1.jpg" alt="">
+                                </div>
+                                <div class="teamTeaxt">
+                                    <h2 class="subheading2">Jihad R. Taylor</h2>
+                                    <div>Chief Executive</div>
+                                    <div class="color_dots mt10">
+                                        <span class="green"></span>
+                                        <span class="yellow"></span>
+                                        <span class="red"></span>
+                                        <span class="blue"></span>
+                                    </div>
+                                </div>
+                            </div>
+                        </li>
+                        <li class="col33">
+                            <div class="teamBox">
+                                <div class="teamImg">
+                                    <img src="<?php echo get_template_directory_uri();?>/images/team2.jpg" alt="">
+                                </div>
+                                <div class="teamTeaxt">
+                                    <h2 class="subheading2">Datesh Kshatriya</h2>
+                                    <div>Chief Financial Officer</div>
+                                    <div class="color_dots mt10">
+                                        <span class="green"></span>
+                                        <span class="yellow"></span>
+                                        <span class="red"></span>
+                                        <span class="blue"></span>
+                                    </div>
+                                </div>
+                            </div>
+                        </li>
+                        <li class="col33">
+                            <div class="teamBox">
+                                <div class="teamImg">
+                                    <img src="<?php echo get_template_directory_uri();?>/images/team3.jpg" alt="">
+                                </div>
+                                <div class="teamTeaxt">
+                                    <h2 class="subheading2">Niresh Agarwal</h2>
+                                    <div>Product & Technology Adviser/Architect</div>
+                                    <div class="color_dots mt10">
+                                        <span class="green"></span>
+                                        <span class="yellow"></span>
+                                        <span class="red"></span>
+                                        <span class="blue"></span>
+                                    </div>
+                                </div>
+                            </div>
+                        </li>
+                        <li class="col33">
+                            <div class="teamBox">
+                                <div class="teamImg">
+                                    <img src="<?php echo get_template_directory_uri();?>/images/team4.jpg" alt="">
+                                </div>
+                                <div class="teamTeaxt">
+                                    <h2 class="subheading2">Michael Prihoda</h2>
+                                    <div>Chief Editor</div>
+                                    <div class="color_dots mt10">
+                                        <span class="green"></span>
+                                        <span class="yellow"></span>
+                                        <span class="red"></span>
+                                        <span class="blue"></span>
+                                    </div>
+                                </div>
+                            </div>
+                        </li>
+                        <li class="col33">
+                            <div class="teamBox">
+                                <div class="teamImg">
+                                    <img src="<?php echo get_template_directory_uri();?>/images/team5.jpg" alt="">
+                                </div>
+                                <div class="teamTeaxt">
+                                    <h2 class="subheading2">Ricardo Martinez</h2>
+                                    <div>Chief UI/UX Designer</div>
+                                    <div class="color_dots mt10">
+                                        <span class="green"></span>
+                                        <span class="yellow"></span>
+                                        <span class="red"></span>
+                                        <span class="blue"></span>
+                                    </div>
+                                </div>
+                            </div>
+                        </li>
+                        <li class="col33">
+                            <div class="teamBox">
+                                <div class="teamImg">
+                                    <img src="<?php echo get_template_directory_uri();?>/images/team6.jpg" alt="">
+                                </div>
+                                <div class="teamTeaxt">
+                                    <h2 class="subheading2">Hassan Ibrahim</h2>
+                                    <div>Marketing Creative</div>
+                                    <div class="color_dots mt10">
+                                        <span class="green"></span>
+                                        <span class="yellow"></span>
+                                        <span class="red"></span>
+                                        <span class="blue"></span>
+                                    </div>
+                                </div>
+                            </div>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+            
+            <div class="abtSection mt80">
+                <div class="abtImg"><img src="<?php echo get_template_directory_uri();?>/images/about1.jpg" alt="" /></div>
+                <div class="abtText">
+                    <h2 class="heading">Your One Stop<br> Shop For Cultural Content</h2>
+                    <p>The Company’s products and participating 3rd party shops are grouped by culture—national, local, and ethnic, as well as various social categories including hip-hop, entrepreneurship, business and religion.</p>
+                    <p>Products available on the site include books, music, film, entertainment, clothing, art, electronics, crafts, collectables, and more.</p>
+                </div>
+                <div class="clear"></div>
+            </div>
+            
+			<div class="innerContainer mt80">
+				<div class="heading center">The Core Four! <br>Making Us A Better Marketplace</div>
+                <div class="coreFour">
+                    <div class="coreFourImg">
+                        <img src="<?php echo get_template_directory_uri();?>/images/coreFourArrow.png" alt="">
+                        <img src="<?php echo get_template_directory_uri();?>/images/coreFour.png" alt="">
+                    </div>
+                    <ul class="ul row">
+                        <li class="col50">
+                            <div class="aboutIconBox">
+                                <div class="aiIcon">
+                                    <img src="<?php echo get_template_directory_uri();?>/images/icon_learn.png" alt="">
+                                </div>
+                                <div class="subheading">Learn Cultures</div>
+                                <div class="aiText">
+                                    <p>Our world is full of cultures.<br>Experience first-hand what this diversity has to offer</p>
+                                </div>
+                                <div class="clear"></div>
+                            </div>
+                        </li>
+                        <li class="col50">
+                            <div class="aboutIconBox">
+                                <div class="aiIcon">
+                                    <img src="<?php echo get_template_directory_uri();?>/images/icon_24_7_security.png" alt="">
+                                </div>
+                                <div class="subheading">Secure Transactions</div>
+                                <div class="aiText">
+                                    <p>Collect, send, and receive payments with confidence. keeping you safe from bad transactions</p>
+                                </div>
+                                <div class="clear"></div>
+                            </div>
+                        </li>
+                        <li class="col50">
+                            <div class="aboutIconBox">
+                                <div class="aiIcon">
+                                    <img src="<?php echo get_template_directory_uri();?>/images/icon_sell.png" alt="">
+                                </div>
+                                <div class="subheading">Sell On Our Platform</div>
+                                <div class="aiText">
+                                    <p>Narrow down your audience with our unique targeted seller options and insights</p>
+                                </div>
+                                <div class="clear"></div>
+                            </div>
+                        </li>
+                        <li class="col50">
+                            <div class="aboutIconBox">
+                                <div class="aiIcon">
+                                    <img src="<?php echo get_template_directory_uri();?>/images/icon_card.png" alt="">
+                                </div>
+                                <div class="subheading">Shop Among Cultures</div>
+                                <div class="aiText">
+                                    <p>Explore new, old or custom design products specifically from your favourite cultures</p>
+                                </div>
+                                <div class="clear"></div>
+                            </div>
+                        </li>
+                    </ul>
+                </div>
+			</div>
+			
+            <div class="abtSection odd mt80">
+                <div class="abtImg"><img src="<?php echo get_template_directory_uri();?>/images/about2.jpg" alt="" /></div>
+                <div class="abtText">
+                    <h2 class="heading">Check Mate Your Competitors! Make Your Next Move Your Best Move</h2>
+                    <ul class="bullet bullet2">
+                        <li>We can help grow your business. Our platform is specifically designed to make your online shop a success!</li>
+                        <li>We make targeting a breeze. With millions of daily users interacting on our site, you are sure to make a storm.</li>
+                        <li>Fulfil your entrepreneurial dreams and reach stardom. Our 24/7 support team is on hand to help every step of the way.</li>
+                    </ul>
+                </div>
+                <div class="clear"></div>
+                <div class="text-center mt40">
+                    <p>At Cultrahub we believe in diversity, teamwork, and fostering partnerships among our Cultrahub community. No other online marketplace is as dedicated to connecting its users with impactful charity organizations working to make real differences in the highest areas of global need. Thank you for visiting us and taking part in this mission in helping create a better world by educating, social involvement, and increasing economic opportunities for all.</p>
+                </div>
+            </div>
+			
+			<div class="innerContainer mt80">
 				<div class="quote">The world’s first ecommerce marketplace designed to facilitate cross-cultural interactions through the exchange of products, services, and information.</div>
 			</div>
 			<hr class="mt50 mb0">
