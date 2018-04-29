06a8a587610075066e6b923f1594e6f688f8a9ac
Mitkar Ghosh <100websolution@gmail.com>
2018-04-29 16:08:57 +0530

    hi

--- wp-content/themes/cultrahub/template-learn.php
+++ wp-content/themes/cultrahub/template-learn.php
@@ -3,6 +3,18 @@
 Template Name: Learn
 */
 get_header('culture');
+$banner_image				= get_field( 'learn_banner_image', $post->ID );
+
+//Section 1
+$learn_page_heading 		= get_field( 'learn_page_heading', $post->ID );
+$learn_page_decription 		= get_field( 'learn_page_decription', $post->ID );
+$learn_page_section_1		= get_field( 'learn_page_section_1', $post->ID );
+//Section 2
+$learn_page_posts			= get_field( 'learn_page_posts', $post->ID );
+//Section 3
+$learn_section_3_heading	= get_field( 'learn_section_3_heading', $post->ID );
+$learn_section_3_description= get_field( 'learn_section_3_description', $post->ID );
+$learn_page_section3_posts	= get_field( 'learn_page_section3_posts', $post->ID );
 ?>
 <!--MAIN CONTAINER START-->
 <div class="mainContainer" id="mainContainer">	
@@ -16,7 +28,7 @@ get_header('culture');
 					<span class="b_red"></span>
 					<span class="b_yellow"></span>
 				</div>
-				<img src="<?php echo get_template_directory_uri();?>/images/learn_banner.jpg" alt="" />
+				<img src="<?php echo $banner_image['url'];?>" alt="" />
 				<div class="border_line">
 					<span class="b_green"></span>
 					<span class="b_blue"></span>
@@ -37,128 +49,97 @@ get_header('culture');
                         </div>
                     </div>
                     <div>
-                        <h2 class="heading center">Ready To Learn Something New?</h2>
-                        <div class="heading_tag">We’ll there’s no better time than the present. We have carefully curated our cultural content to bring you the very best articles, news, and video clips from top cultural leaders, businessmen, and Producers in your community. Let’s learn, share and grow!</div>
+                        <h2 class="heading center"><?php echo $learn_page_heading;?></h2>
+                        <div class="heading_tag"><?php echo $learn_page_decription;?></div>
+					<?php
+					if(!empty($learn_page_section_1)){
+					?>
                         <div class="seller_tool_wrap withdot w75 mlrauto">
                             <ul class="row ul">
+							<?php
+							foreach($learn_page_section_1 as $post1){
+							?>
                                 <li class="col33">
-                                    <div class="seller_block green">
-                                        <div class="seller_icon"><img src="<?php echo get_template_directory_uri();?>/images/icon_fav_culture.png" alt=""></div>
+                                    <div class="seller_block <?php if($post1['learn_page_section1_heading']!='')echo $post1['learn_page_section1_color'];?>">
+                                        <div class="seller_icon"><img src="<?php echo $post1['learn_page_section1_image']['url'];?>" alt=""></div>
                                         <div class="seller_text">
-                                            <h3 class="subheading2">Click On Your Favourite Cultures</h3>
+                                            <h3 class="subheading2"><?php echo $post1['learn_page_section1_heading'];?></h3>
                                         </div>
                                     </div>
                                 </li>
-                                <li class="col33">
-                                    <div class="seller_block yellow">
-                                        <div class="seller_icon"><img src="<?php echo get_template_directory_uri();?>/images/icon_article.png" alt=""></div>
-                                        <div class="seller_text">
-                                            <h3 class="subheading2">Search Through Our Huge Range Of Articles And Videos</h3>
-                                        </div>
-                                    </div>
-                                </li>
-                                <li class="col33">
-                                    <div class="seller_block blue">
-                                        <div class="seller_icon"><img src="<?php echo get_template_directory_uri();?>/images/icon_like.png" alt=""></div>
-                                        <div class="seller_text">
-                                            <h3 class="subheading2">Comment, <br>Like Or Share!</h3>
-                                        </div>
-                                    </div>
-                                </li>
+                            <?php
+							}
+							?>
                             </ul>
                         </div>
+					<?php
+					}
+					?>
                     </div>
                 </div>
 			</div>
-			<hr class="mt70 mb70">
+			<hr class="mt70 mb70">		
 			<div class="innerContainer">
+			<?php
+			if(!empty($learn_page_posts)){
+			?>
                 <div class="learnList">
                     <ul class="ul row">
+					<?php
+					foreach($learn_page_posts as $post2){
+					?>
                         <li class="col50">
                             <div class="learnBox">
                                 <div class="learnImg">
-                                    <img src="<?php echo get_template_directory_uri();?>/images/learn1.jpg" alt="">
+                                    <img src="<?php echo $post2['learn_post_image']['url'];?>" alt="">
                                 </div>
                                 <div class="learnText">
-                                    <h3 class="heading nobrdr">Articles, Blogs, And Interviews</h3>
-                                    <p>Browse learn and share the most current trending news, articles and issues around the cultures. Create and Publish your own article to start discussions around a topic you feel needs to be discussed.</p>
+                                    <h3 class="heading nobrdr"><?php echo $post2['learn_post_title'];?></h3>
+                                    <p><?php echo $post2['learn_post_description'];?></p>
                                 </div>
                                 <div class="clear"></div>
                             </div>
                         </li>
-                        <li class="col50">
-                            <div class="learnBox">
-                                <div class="learnImg">
-                                    <img src="<?php echo get_template_directory_uri();?>/images/learn2.jpg" alt="">
-                                </div>
-                                <div class="learnText">
-                                    <h3 class="heading nobrdr">Share A Quote Or Create Your Own</h3>
-                                    <p>Users can read influential quotes from renown cultural influencers and add words of wisdom to their arsenal. Find, create or share yours today.</p>
-                                </div>
-                                <div class="clear"></div>
-                            </div>
-                        </li>
-                        <li class="col50">
-                            <div class="learnBox">
-                                <div class="learnImg">
-                                    <img src="<?php echo get_template_directory_uri();?>/images/learn3.jpg" alt="">
-                                </div>
-                                <div class="learnText">
-                                    <h3 class="heading nobrdr">In-Depth Bio’s From Cultural Leaders</h3>
-                                    <p>Read and learn about legendary cultural influencers daily. Read about their struggles, accomplishments, and values.</p>
-                                </div>
-                                <div class="clear"></div>
-                            </div>
-                        </li>
-                        <li class="col50">
-                            <div class="learnBox">
-                                <div class="learnImg">
-                                    <img src="<?php echo get_template_directory_uri();?>/images/learn4.jpg" alt="">
-                                </div>
-                                <div class="learnText">
-                                    <h3 class="heading nobrdr">Listen, Read And Learn From Top Cultural Scholars</h3>
-                                    <p>Follow your favorite scholars and join their discussions and classes covering numerous subjects including Religion, Health & wellness, History, and business.</p>
-                                </div>
-                                <div class="clear"></div>
-                            </div>
-                        </li>
+					<?php
+					}
+					?>
                     </ul>
                 </div>
+			<?php
+			}
+			?>
                 
 			    <div class="mt100">
-                    <h2 class="heading center">Publish Your Content On Cultrahub</h2>
-                    <div class="heading_tag">Got something to say? Set up your page and publish your own content. Reach friends, family and millions of followers around the world with the help of Cultrahub. Let’s go viral!</div>
+                    <h2 class="heading center"><?php echo $learn_section_3_heading;?></h2>
+                    <div class="heading_tag"><?php echo $learn_section_3_description;?></div>
+				<?php
+				if(!empty($learn_page_section3_posts)){
+				?>
                     <div class="seller_tool_wrap w75 mlrauto">
                         <ul class="row ul">
+						<?php
+						foreach($learn_page_section3_posts as $post3){
+						?>
                             <li class="col33">
                                 <div class="seller_block">
-                                    <div class="seller_icon"><img src="<?php echo get_template_directory_uri();?>/images/icon_setup_page.png" alt=""></div>
+                                    <div class="seller_icon"><img src="<?php echo $post3['learn_post_section3_image']['url'];?>" alt=""></div>
                                     <div class="seller_text">
-                                        <h3 class="subheading2">Set Up Your <br>Page</h3>
+                                        <h3 class="subheading2"><?php echo $post3['learn_post_section3_title'];?></h3>
                                     </div>
                                 </div>
                             </li>
-                            <li class="col33">
-                                <div class="seller_block">
-                                    <div class="seller_icon"><img src="<?php echo get_template_directory_uri();?>/images/icon_post_blog.png" alt=""></div>
-                                    <div class="seller_text">
-                                        <h3 class="subheading2">Post Your <br>Videos And Blogs</h3>
-                                    </div>
-                                </div>
-                            </li>
-                            <li class="col33">
-                                <div class="seller_block">
-                                    <div class="seller_icon"><img src="<?php echo get_template_directory_uri();?>/images/icon_follower.png" alt=""></div>
-                                    <div class="seller_text">
-                                        <h3 class="subheading2">Watch As Your Followers Grow</h3>
-                                    </div>
-                                </div>
-                            </li>
+						<?php
+						}
+						?>
                         </ul>
                     </div>
+				<?php
+				}
+				?>
                 </div>
 			</div>
 			<hr class="mt70 mb0">
+		
 		</div>
 	</div>
 	
